c071c9d095aa47f1b339ee09b27343f9ea001ee7
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2024-01-23 12:42:31 +0100

    added translation resources

--- examenporject_Frameworks_zenodesp/Areas/Identity/Data/EmployeeUser.cs
+++ examenporject_Frameworks_zenodesp/Areas/Identity/Data/EmployeeUser.cs
@@ -10,6 +10,7 @@ namespace examenporject_Frameworks_zenodesp.Areas.Identity.Data
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name ="Department")]
         public string? Department { get; set; }
     }
 }
